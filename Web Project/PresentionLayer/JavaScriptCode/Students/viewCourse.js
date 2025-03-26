c5d4aef2c72e53494c6f558305e11a88f3ef3098
abdelrahman1425376 <155455980+abdelrahman1425376@users.noreply.github.com>
2025-03-26 15:00:43 +0300

    update

--- Web Project/PresentionLayer/JavaScriptCode/Students/viewCourse.js
+++ Web Project/PresentionLayer/JavaScriptCode/Students/viewCourse.js
@@ -13,8 +13,31 @@ async function assginUserName()
     const welcomeMessage = document.getElementById('UserName');
     welcomeMessage.textContent = ` ${username}`;  
 }
+   
+function displayCourses(courses,id) {
+    let container = document.getElementById(id);
+    container.innerHTML = ''; 
+    courses.forEach(course => {    
+        container.innerHTML += `
+         <li>${course.course_name} - Grade: ${course.grade}</li>
+        `;
+    });
+   
+}
+
+async function loadCompletedCourses() {
+           
+    const storage = localStorage.getItem('registerration');
+if (storage) {
+    displayCourses(JSON.parse(storage).filter(e=>e.statusRegster==1),"completed");
+    displayCourses(JSON.parse(storage).filter(e=>e.statusRegster==2),"InProgress");
+    displayCourses(JSON.parse(storage).filter(e=>e.statusRegster==3),"Pending");
+
+} 
+}
+
 async function initalScreen() {
-    // loadCourses();
-    // loadRegister();
+    loadCompletedCourses();
+  
     assginUserName();
 }
